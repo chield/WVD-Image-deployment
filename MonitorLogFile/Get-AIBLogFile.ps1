83cda4a698a4a48784afde8d4b9bbb819521bcb1
Michiel Dekker <dekker.michiel@outlook.com>
2021-02-23 23:03:35 +0100

    changes

--- MonitorLogFile/Get-AIBLogFile.ps1
+++ MonitorLogFile/Get-AIBLogFile.ps1
@@ -7,7 +7,7 @@
         $s = Get-AzStorageAccount -ResourceGroupName $r.ResourceGroupName -Name $StorageAccountName
         $p = Get-AzStorageContainer -Context $s.context | Where-Object { $_.Name -eq 'packerlogs' }
         $container = Get-AzStorageBlob -Container $p.Name -Context $s.context
-        $fileLoc = Get-AzStorageBlobContent -Container $p.Name -Context $s.context -Blob $container.Name -Destination 'c:\Temp\' -Force -ErrorAction SilentlyContinue
+        $fileLoc = Get-AzStorageBlobContent -Container $p.Name -Context $s.context -Blob $container.Name -Destination 'c:\Temp' -Force -ErrorAction SilentlyContinue
         Copy-Item ( Join-Path 'c:\Temp' $fileLoc.name) $TargetPath -Force -ErrorAction SilentlyContinue
         $content = Get-Content -Path $TargetPath -Tail 1
         if ($content -like "*Done exporting Packer logs to Azure Storage.") {
