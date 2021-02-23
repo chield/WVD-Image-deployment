e9211e82bd6f2e34db0176c1f02c48a60d442aca
Michiel Dekker <dekker.michiel@outlook.com>
2021-02-23 10:48:33 +0100

    changes

--- MonitorLogFile/Get-AIBLogFile.ps1
+++ MonitorLogFile/Get-AIBLogFile.ps1
@@ -1,22 +1,9 @@
-function Get-AibLogFile {
-    param(
-        [string]$StorageAccountName,
-        [string]$TargetPath = 'C:\Temp\customization.log'
-    )
-    $continue = $true
-    While ($continue) {
-        $r = Get-AzResourceGroup -Tag @{createdby = 'AzureVMImageBuilder' }
-        $s = Get-AzStorageAccount -ResourceGroupName $r.ResourceGroupName -Name $StorageAccountName
-        $p = Get-AzStorageContainer -Context $s.context | Where-Object { $_.Name -eq 'packerlogs' }
-        $container = Get-AzStorageBlob -Container $p.Name -Context $s.context
-        $fileLoc = Get-AzStorageBlobContent -Container $p.Name -Context $s.context -Blob $container.Name -Destination 'c:\Temp' -Force -ErrorAction SilentlyContinue
-        Copy-Item ( Join-Path 'c:\Temp' $fileLoc.name) $TargetPath -Force -ErrorAction SilentlyContinue
-        $content = Get-Content -Path $TargetPath -Tail 1
-        if ($content -like "*Done exporting Packer logs to Azure Storage.") {
-            $continue = $false
-            Write-Output $content
-        }
-    }
-}
+$StorageAccountName = 'q7fplpn3pg91v9yhcacpuux6'
+$TargetPath = 'C:\Temp\customization.log'
+$r = Get-AzResourceGroup -Tag @{createdby = 'AzureVMImageBuilder' }
+$s = Get-AzStorageAccount -ResourceGroupName $r.ResourceGroupName -Name $StorageAccountName
+$p = Get-AzStorageContainer -Context $s.context | Where-Object { $_.Name -eq 'packerlogs' }
+$container = Get-AzStorageBlob -Container $p.Name -Context $s.context
+Get-AzStorageBlobContent -Container $p.Name -Context $s.context -Blob $container.Name -Destination $TargetPath -Force -ErrorAction SilentlyContinue
 
 
