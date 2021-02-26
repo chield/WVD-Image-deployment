159f138309a41a8d837fdc8241dbe4e69356b26c
Michiel Dekker <dekker.michiel@outlook.com>
2021-02-26 18:27:20 +0100

    changes

---
+++ AddVM2Hostpool/RdsRegistrationInfotoken.ps1
@@ -0,0 +1,26 @@
+# ARM RdsRegistrationInfotoken Used by arm parameter hostpoolToken
+# Get RdsRegistrationInfotoken
+# Import Modules for WVD
+
+import-module az.desktopvirtualization
+import-module az.network
+import-module az.compute
+
+$azureSubscriptionID = "60793049-d642-4e61-ab84-c16ae308b6ca"
+$resourceGroupName = "rg-weu-devops"
+$existingWVDHostPoolName = "WVD4All-Acceptatie"
+
+#Obtain RdsRegistrationInfotoken
+
+$Registered = Get-AzWvdRegistrationInfo -SubscriptionId "$azureSubscriptionID" -ResourceGroupName "$resourceGroupName" -HostPoolName $existingWVDHostPoolName
+if (-not(-Not $Registered.Token)){$registrationTokenValidFor = (NEW-TIMESPAN -Start (get-date) -End $Registered.ExpirationTime | select Days,Hours,Minutes,Seconds)}
+
+$registrationTokenValidFor
+if ((-Not $Registered.Token) -or ($Registered.ExpirationTime -le (get-date)))
+{
+    $Registered = New-AzWvdRegistrationInfo -SubscriptionId $azureSubscriptionID -ResourceGroupName $resourceGroupName -HostPoolName $existingWVDHostPoolName -ExpirationTime (Get-Date).AddHours(4) -ErrorAction SilentlyContinue
+}
+$RdsRegistrationInfotoken = $Registered.Token
+
+# Write Host task.setvariable 
+Write-Host "##vso[task.setvariable variable=RdsRegistrationInfotoken;]$RdsRegistrationInfotoken"
